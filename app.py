b9786c29cba198bf39dfc5c0abfa818cf11c8af9
Nana Kweku <kweku@Nana-Kwekus-MacBook-Pro.local>
2021-02-12 06:02:11 +0000

    PROGRESS

--- app.py
+++ app.py
@@ -1,13 +1,18 @@
 from flask import Flask, render_template, redirect, flash, url_for
 from flask_sqlalchemy import SQLAlchemy
-from forms import RegistrationForm, ItemForm
+from forms import RegistrationForm, ItemForm, LoginForm
+from flask_login import UserMixin, login_user, current_user
 import secrets
 import os
+
+from flask_login import LoginManager
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI']='sqlite:///test.db'
 app.config['SECRET_KEY'] = '5791628bb0b13ce0c676dfde280ba245'
 db = SQLAlchemy(app)
+login_manager = LoginManager(app)
+
 
 class Item(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -17,15 +22,20 @@ class Item(db.Model):
     image =  db.Column (db.String(60), default='default.jpg')
     trending = db.Column (db.Boolean, default = False)
     # user = db.Column(db.Integer, db.ForeignKey('user.id', ondelete="CASCADE"), nullable=False)
-    
+    vendor = db.Column(db.Integer, db.ForeignKey('user.id', ondelete="CASCADE"), nullable=False)
 def __repr__(self): 
     return'<Item %r>' % self.name
 
-class User(db.Model):
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(user_id)
+
+class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(10), nullable = False)
     phone = db.Column(db.String(15), nullable=False)
     password = db.Column(db.String(15))
+    stock = db.relationship('Item', backref='author', lazy=True)
 
 def __repr__(self):
     return '<User %r' % self.username
@@ -44,15 +54,19 @@ def save_picture(form_picture):
     return picture_fn
 
 
-@app.route('/')
+@app.route('/', methods=['POST','GET'])
 def index():
-    items = Item.query.all()
+    items = Item.query.order_by(Item.id.desc()).all()
+    user = current_user
     return render_template('index.html', items = items)
+
 
 @app.route('/preview/<int:itemid>')
 def preview(itemid):
     item = Item.query.filter_by(id=itemid).first()
-    return render_template('preview.html',item=item)
+    vendor = User.query.filter_by(id = item.vendor).first()
+    vendorname = vendor.username
+    return render_template('preview.html', item=item, vendorname=vendorname, vendor=vendor)
 
 @app.route('/additem', methods=['POST','GET'])
 def additem():
@@ -62,12 +76,19 @@ def additem():
         if form.picture.data:
             print('YO!!!!!!!!! IT IS OVER HERE!!!')
             pic = save_picture(form.picture.data)
-        new_item = Item(name = form.name.data, price = form.price.data, image=pic, description = form.description.data)
+        new_item = Item(name = form.name.data, price = form.price.data, image=pic, description = form.description.data, vendor = current_user.id)
         db.session.add(new_item)
         db.session.commit()
         flash(f'New Item has been added', 'success')
+        return redirect(url_for('account'))
     return render_template('addpost.html', form=form)
 
+@app.route('/test', methods=['POST','GET'])
+def test():
+    return render_template('asdf.html')
+
+# @app.route('/myitems')
+# def myitems():
 
 @app.route('/register', methods=['POST','GET'])
 def register():
@@ -79,6 +100,30 @@ def register():
         flash (f'Account for ' + form.username.data + ' has been created.', 'error') 
         return redirect (url_for('index'))
     return render_template('register.html',  form=form)
+
+@app.route('/account')
+def account():
+    user = current_user
+    return render_template('account.html', user=user)
+
+@app.route('/allusers')
+def allusers():
+    allusers = User.query.all()
+    return render_template('allusers.html', allusers=allusers)
+
+@app.route('/login',methods=['POST','GET'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+         # Login and validate the user.
+        # user should be an instance of your `User` class
+        user = User.query.filter_by(phone=form.phone.data).first()
+        if user and user.password==form.password.data:
+            login_user(user)
+            print ("Logged in:" + user.username + " " + user.phone)
+            print(form.password.data)
+        return redirect(url_for('index'))
+    return render_template('login.html', form=form)
 
 if __name__ == '__main__':
     app.run(debug=True)
--- forms.py
+++ forms.py
@@ -5,7 +5,6 @@ from flask_wtf.file import FileField, FileAllowed
 from wtforms.widgets import TextArea
 from flask_wtf.file import FileField, FileAllowed
 
-
 class RegistrationForm(FlaskForm):
     username = StringField('Name', validators=[DataRequired()])
     phone = StringField('Phone', validators=[DataRequired()])
@@ -17,6 +16,10 @@ class ItemForm(FlaskForm):
     name = StringField('Name', validators=[DataRequired()])
     price = StringField('Price', validators=[DataRequired()])
     description = StringField('Description', validators=[DataRequired()])
-    picture = FileField('Add a picture', validators=[FileAllowed(['jpg', 'png'])])
+    picture = FileField('Add a picture', validators=[DataRequired() , FileAllowed(['jpg', 'png'])])
     submit = SubmitField('Add Item')
 
+class LoginForm(FlaskForm):
+    phone = StringField('Phone', validators=[DataRequired()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    submit = SubmitField('Login')
